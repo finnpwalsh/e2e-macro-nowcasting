1e78bf71181ce1ae295eec46a041bb72ab44c798
Finn Walsh <fpw5@pitt.edu>
2025-12-27 10:03:13 -0500

    fix: use M instead of ME to denote month-end

--- src/utils/time.py
+++ src/utils/time.py
@@ -10,7 +10,7 @@ def to_month_index(df: pd.DataFrame) -> pd.DataFrame:
     out = out.set_index("date")
     out.index = (
         pd.to_datetime(out.index)
-        .to_period("ME")
-        .to_timestamp("ME")
+        .to_period("M")
+        .to_timestamp("M")
     )
     return out
