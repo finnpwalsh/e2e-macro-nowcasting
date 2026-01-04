3f9288586c52a28b06ef6548fa5ecb21a24d34e1
Finn Walsh <fpw5@pitt.edu>
2026-01-04 16:59:06 -0500

    refactor: move metrics schema to baseline schema

--- src2/train/baseline/schema.py
+++ src2/train/baseline/schema.py
@@ -3,3 +3,17 @@ BASELINE_TRAIN_PRED_COLS = [
     "y",
     "y_hat",
 ]
+
+REGRESSION_METRICS = [
+    "rmse", 
+]
+
+RUN_METADATA_KEYS = [
+    "split_date", 
+    "n_train", 
+    "n_valid", 
+    "n_feats",
+    "target",
+]
+
+BASELINE_RUN_REQUIRED_KEYS = REGRESSION_METRICS + RUN_METADATA_KEYS
