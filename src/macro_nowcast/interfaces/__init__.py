9bc72ccedecdb54b601f24aa845a6b65a3d01b18
Finn Walsh <fpw5@pitt.edu>
2026-02-14 17:40:35 -0500

    chore: update inits

--- src/macro_nowcast/interfaces/__init__.py
+++ src/macro_nowcast/interfaces/__init__.py
@@ -1,9 +1,13 @@
+from .assembler import Assembler
 from .canonicalizer import Canonicalizer
 from .contract import Contract
-from .providers import Provider
+from .feature_builder import FeatureBuilder
+from .provider import Provider
 
 __all__ = [
+    "Assembler",
     "Canonicalizer",
     "Contract",
     "Provider",
+    "FeatureBuilder",
 ]
---
+++ src/macro_nowcast/prepare/anchors/__init__.py
@@ -0,0 +1,9 @@
+from .assemble import AnchorAssembler
+from .contract import CONTRACT
+from .features import AnchorFeatureBuilder
+
+__all__ = [
+    "AnchorAssembler",
+    "CONTRACT",
+    "AnchorFeatureBuilder",
+]
