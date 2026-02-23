c5b629b1c7d76dc8efe530fcc77f96370f203e61
Finn Walsh <fpw5@pitt.edu>
2026-02-23 17:49:37 -0500

    feat: add secrets module to dev main

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -2,12 +2,28 @@ module "ssm_config" {
     source = "../../modules/ssm_parameters"
     project = "nowcasting"
     env = "dev"
+
+    values = {
+        MLFLOW_TRACKING_URI = "http://mlflow:5000"
+        MLFLOW_EXPERIMENT_NAME = "nowcasting"
+        NOWCAST_REGISTRY_NAME = "nowcasting-models"
+        NOWCAST_MODEL_ALIAS = "champion"
+        STORAGE_BACKEND = "S3"
+    }
 }
 
-values = {
-    MLFLOW_TRACKING_URI = "http://mlflow:5000"
-    MLFLOW_EXPERIMENT_NAME = "nowcasting"
-    NOWCAST_REGISTRY_NAME = "nowcasting-models"
-    NOWCAST_MODEL_ALIAS = "champion"
-    STORAGE_BACKEND = "S3"
+module "secrets" {
+    source = "../../modules/secrets"
+    project = "nowcasting"
+    env = "dev"
+
+    keys = [
+        "FRED_API_KEY",
+        "TIINGO_API_KEY",
+        "AIRFLOW__WEBSERVER__SECRET_KEY",
+        "AIRFLOW__CORE__FERNET_KEY",
+        "AIRFLOW_ADMIN_PASSWORD",
+        "AIRFLOW_DB_URI",
+        "MLFLOW_BACKEND_STORE_URI",
+    ]
 }
