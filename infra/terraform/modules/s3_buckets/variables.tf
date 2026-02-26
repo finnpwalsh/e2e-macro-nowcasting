75e8b44ab1d7f4616488b0ccbdcdd6e3ec62c1b1
Finn Walsh <fpw5@pitt.edu>
2026-02-26 09:49:47 -0500

    chore: remove defaults from s3 buckets

--- infra/terraform/modules/s3_buckets/variables.tf
+++ infra/terraform/modules/s3_buckets/variables.tf
@@ -1,17 +1,5 @@
-variable "aws_region" {
-  description = "AWS region for infrastructure"
-  type        = string
-  default     = "us-east-1"
-}
+variable "aws_region" { type = string }
 
-variable "project" {
-  description = "Project name prefix"
-  type        = string
-  default     = "nowcasting"
-}
+variable "project" { type = string }
 
-variable "env" {
-  description = "Environment name (dev | prod)"
-  type        = string
-  default     = "dev"
-}
+variable "env" { type = string }
