1fd02a947dcfd5b70027c5b75d4ec401a4998cc8
Finn Walsh <fpw5@pitt.edu>
2026-02-27 10:15:28 -0500

    feat: add ecr repositories

---
+++ infra/terraform/modules/ecr_repositories/main.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "this" {
+    for_each = toset(var.repositories)
+    
+    name = "${var.project}-${var.env}-${each.key}"
+    image_tag_mutability = "MUTABLE"
+
+    image_scanning_configuration { scan_on_push = true}
+}
