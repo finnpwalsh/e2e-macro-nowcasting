083b635509a43a706b8f1feff2cf3668e514ebc6
Finn Walsh <fpw5@pitt.edu>
2026-03-02 18:57:02 -0500

    fix: tasks -> runtimes

--- infra/terraform/modules/runtimes/outputs.tf
+++ infra/terraform/modules/runtimes/outputs.tf
@@ -1,19 +1,19 @@
 output "runtime_role_arns" {
-    value = { for k, r in aws_iam_role.task : k => r.arn }
+    value = { for k, r in aws_iam_role.runtime : k => r.arn }
 }
 
 output "runtime_role_names" {
-    value = { for k, r in aws_iam_role.task : k => r.name }
+    value = { for k, r in aws_iam_role.runtime : k => r.name }
 }
 
 output "ecr_repo_urls" {
-    value = { for k, r in aws_ecr_repository.task : k => r.repository_url }
+    value = { for k, r in aws_ecr_repository.runtime : k => r.repository_url }
 }
 
 output "log_group_arns" {
-    value = { for k, lg in aws_cloudwatch_log_group.task : k => lg.arn }
+    value = { for k, lg in aws_cloudwatch_log_group.runtime : k => lg.arn }
 }
 
 output "log_group_names" {
-    value = { for k, lg in aws_cloudwatch_log_group.task : k => lg.name }
+    value = { for k, lg in aws_cloudwatch_log_group.runtime : k => lg.name }
 }
