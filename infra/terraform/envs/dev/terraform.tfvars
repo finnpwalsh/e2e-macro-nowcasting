3426ffe61ba0e586a117f9061d2192087f7001db
Finn Walsh <fpw5@pitt.edu>
2026-03-02 10:11:58 -0500

    refactor: remove over-modular main

--- infra/terraform/envs/dev/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-vpc_id = "vpc-0abfeeed55a595fd6"
-
-subnet_ids = [
-  "subnet-01177eafb413dc472",
-  "subnet-0c06b8f2b4a5e2bfa",
-]
--- infra/terraform/envs/dev/variables.tf
+++ infra/terraform/envs/dev/variables.tf
@@ -12,16 +12,3 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "subnet_ids" {
-  type = list(string)
-}
-
-variable "vpc_cidr" {
-  type    = string
-  default = "10.20.0.0/16"
-}
