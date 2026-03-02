20f355b52277209d366b12f2b9e8b7f3610bf278
Finn Walsh <fpw5@pitt.edu>
2026-03-02 09:07:07 -0500

    fix: remove private subnets

--- infra/terraform/modules/vpc/main.tf
+++ infra/terraform/modules/vpc/main.tf
@@ -28,20 +28,8 @@ resource "aws_subnet" "public_b" {
     map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private_a" {
-    vpc_id = aws_vpc.this.id
-    availability_zone = data.aws_availability_zones.available.names[0]
-    cidr_block = "10.20.32.0/20"
-}
-
-resource "aws_subnet" "private_b" {
-    vpc_id = aws_vpc.this.id
-    availability_zone = data.aws_availability_zones.available.names[1]
-    cidr_block = "10.20.48.0/20"
-}
-
 # ------------------------------------------
-# Public Route Table
+# Route Table
 # ------------------------------------------
 
 resource "aws_route_table" "public" {
@@ -49,7 +37,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route" "public_default" {
-    route_table_id         = aws.route_table.public.id
+    route_table_id         = aws_route_table.public.id
     destination_cidr_block = "0.0.0.0/0"
     gateway_id             = aws_internet_gateway.this.id
 }
@@ -62,42 +50,4 @@ resource "aws_route_table_association" "public_a" {
 resource "aws_route_table_association" "public_b" {
     subnet_id      = aws_subnet.public_b.id
     route_table_id = aws_route_table.public.id
-}
-
-# ------------------------------------------
-# NAT
-# ------------------------------------------
-
-resource "aws_eip" "nat" {
-    domain = "vpc"
-}
-
-resource "aws_nat_gateway" "this" {
-    allocation_id = aws_eip.nat.id
-    subnet_id     = aws_subnet.public_a.id
-    depends_on    = [aws_internet_gateway.this]
-}
-
-# ------------------------------------------
-# Private Route Table
-# ------------------------------------------
-
-resource "aws_route_table" "private" {
-    vpc_id = aws_vpc.this.id
-}
-
-resource "aws_route" "private_default" {
-    route_table_id         = aws.route_table.private.id
-    destination_cidr_block = "0.0.0.0/0"
-    nat_gateway_id        = aws_internet_gateway.this.id
-}
-
-resource "aws_route_table_association" "private_a" {
-    subnet_id = aws_subnet.private_a.id
-    route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "private_b" {
-    subnet_id = aws_subnet.private_b.id
-    route_table_id = aws_route_table.private.id
 }
--- infra/terraform/modules/vpc/outputs.tf
+++ infra/terraform/modules/vpc/outputs.tf
@@ -5,13 +5,6 @@ output "vpc_id" {
 output "public_subnet_ids" {
     value = [
         aws_subnet.public_a.id,
-        aws_suvnet.public_b.id
-    ]
-}
-
-output "private_subnet_ids" {
-    value = [
-        aws_subnet.private_a.id,
-        aws_subnet.private_b.id
+        aws_subnet.public_b.id
     ]
 }
