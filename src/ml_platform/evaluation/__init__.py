af67bd17c4de794c71e1d0e81028c88850d36536
Finn Walsh <fpw5@pitt.edu>
2026-03-28 17:49:39 -0400

    fix: update init

--- src/ml_platform/evaluation/__init__.py
+++ src/ml_platform/evaluation/__init__.py
@@ -1,6 +1,8 @@
-from .base import Metrics, Scorer, Evaluator
+from .schema import Metric, Metrics
+from .base import Scorer, Evaluator
 
 __all__ = [
+    "Metric",
     "Metrics",
     "Scorer",
     "Evaluator",
