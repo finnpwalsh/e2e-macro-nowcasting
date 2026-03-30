b104cccd414d1eadb77c11f5f9e2be03242bb32d
Finn Walsh <fpw5@pitt.edu>
2026-03-30 16:46:41 -0400

    chore: remove deprecated files

--- src/ml_platform/modeling/time_series/__init__.py
+++ src/ml_platform/modeling/time_series/__init__.py
@@ -1,14 +1,5 @@
-from .schema import TimeSeriesTrainingConfig, TimeSeriesEvalResult
 from .splitters import TimeSplitter
-from .train import run_time_series_training
-from .evaluate import TimeSeriesEvaluator
-from .track import TimeSeriesTrackingAdapter
 
 __all__ = [
-    "TimeSeriesTrainingConfig",
-    "run_time_series_training",
-    "TimeSeriesEvalResult",
     "TimeSplitter",
-    "TimeSeriesEvaluator",
-    "TimeSeriesTrackingAdapter",
 ]
