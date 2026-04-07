e87abf357049ebfe7a48b1902ad3b8bd2f6d8a9b
Finn Walsh <fpw5@pitt.edu>
2026-04-07 13:19:39 -0400

    refactor: store predictions, residuals logic in artifacts

---
+++ src/ml_platform/artifacts/__init__.py
@@ -0,0 +1,11 @@
+from .predictions import Predictions, PredictionsBuilder, PredictionsResolver
+from .residuals import Residuals, ResidualsBuilder, ResidualsResolver
+
+__all__ = [
+    "Predictions",
+    "PredictionsBuilder",
+    "PredictionsResolver",
+    "Residuals",
+    "ResidualsBuilder",
+    "ResidualsResolver",
+]
