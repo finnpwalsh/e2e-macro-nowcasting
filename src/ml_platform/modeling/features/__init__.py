548627925f62abc3664bfec86c67fff54253b357
Finn Walsh <fpw5@pitt.edu>
2026-04-15 14:54:31 -0400

    fix: move feature builder from datasets to modeling

--- src/ml_platform/modeling/features/__init__.py
+++ src/ml_platform/modeling/features/__init__.py
@@ -1,7 +1,9 @@
+from .builder import FeatureBuilder
 from .selector import FeatureSelector
 from .signature import FeatureSignature, FeatureSignatureBuilder
 
 __all__ = [
+    "FeatureBuilder",
     "FeatureSelector",
     "FeatureSignature",
     "FeatureSignatureBuilder",
