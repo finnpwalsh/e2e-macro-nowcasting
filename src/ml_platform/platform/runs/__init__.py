5f9e0515b16f4f80c84222474dbea73ecf5840e5
Finn Walsh <fpw5@pitt.edu>
2026-04-15 10:08:14 -0400

    fix: remove generic RunSummary + RunManifest... add abstraction without adding value

--- src/ml_platform/platform/runs/__init__.py
+++ src/ml_platform/platform/runs/__init__.py
@@ -1,13 +1,10 @@
 from .context import RunContext
-from .contracts import RunSummary, RunManifest
 from .identity import RunIdentity
 from .keys import RunKeys, PointerKeys
-from .pointers import RunPointer
+from .pointer import RunPointer
 
 __all__ = [
     "RunContext",
-    "RunSummary",
-    "RunManifest",
     "RunIdentity",
     "RunKeys",
     "PointerKeys",
