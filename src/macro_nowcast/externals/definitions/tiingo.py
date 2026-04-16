5655a29b014a3b8342949d51547b5bf83f0ac3aa
Finn Walsh <fpw5@pitt.edu>
2026-04-16 11:09:40 -0400

    refactor: move series, tickers from externals to configs

--- src/macro_nowcast/externals/definitions/tiingo.py
+++
@@ -1,7 +0,0 @@
-TICKERS: dict[str, str] = {
-    "sp500_equity": "SPY",
-    "long_term_treasury": "TLT",
-    "us_dollar_index": "UUP",
-    "energy_sector": "XLE",
-    "high_yield_credit":"HYG",
-}
